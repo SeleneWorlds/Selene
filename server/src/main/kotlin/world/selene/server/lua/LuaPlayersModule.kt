af4ddfa82a55e46c21cd9a120795cba67233a4e0
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-18 13:34:21 +0200

    feat: Add Players#GetOnlinePlayers to Lua

--- server/src/main/kotlin/world/selene/server/lua/LuaPlayersModule.kt
+++ server/src/main/kotlin/world/selene/server/lua/LuaPlayersModule.kt
@@ -1,9 +1,13 @@
 package world.selene.server.lua
 
+import party.iroiro.luajava.Lua
 import party.iroiro.luajava.value.LuaValue
 import world.selene.common.lua.LuaModule
+import world.selene.common.lua.register
+import world.selene.server.network.NetworkServer
+import world.selene.server.network.NetworkClientImpl
 
-class LuaPlayersModule(private val signals: ServerLuaSignals) : LuaModule {
+class LuaPlayersModule(private val signals: ServerLuaSignals, private val networkServer: NetworkServer) : LuaModule {
     override val name = "selene.players"
 
     override fun register(table: LuaValue) {
@@ -11,6 +15,16 @@ class LuaPlayersModule(private val signals: ServerLuaSignals) : LuaModule {
         table.set("PlayerDequeued", signals.playerDequeued)
         table.set("PlayerJoined", signals.playerJoined)
         table.set("PlayerLeft", signals.playerLeft)
+        table.register("GetOnlinePlayers", this::luaGetOnlinePlayers)
+    }
+
+    private fun luaGetOnlinePlayers(lua: Lua): Int {
+        val players = networkServer.clients
+            .filterIsInstance<NetworkClientImpl>()
+            .map { it.player.luaProxy }
+
+        lua.push(players, Lua.Conversion.FULL)
+        return 1
     }
 
 }
