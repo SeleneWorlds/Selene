778b5f39d5bee19e1ef7e4f15376d001b83d3fa5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-28 08:22:39 +0200

    feat: Improve not implemented errors in LuaMetatable

--- common/src/main/kotlin/world/selene/common/lua/LuaMetatable.kt
+++ common/src/main/kotlin/world/selene/common/lua/LuaMetatable.kt
@@ -3,9 +3,9 @@ package world.selene.common.lua
 import party.iroiro.luajava.Lua
 
 interface LuaMetatable {
-    fun luaCall(lua: Lua): Int = lua.error(NotImplementedError())
-    fun luaGet(lua: Lua): Int = lua.error(NotImplementedError())
-    fun luaSet(lua: Lua): Int = lua.error(NotImplementedError())
+    fun luaCall(lua: Lua): Int = lua.throwError("attempt to call a ${luaTypeName()} value")
+    fun luaGet(lua: Lua): Int = lua.throwError("attempt to index a ${luaTypeName()} value")
+    fun luaSet(lua: Lua): Int = lua.throwError("attempt to index a ${luaTypeName()} value")
     fun luaToString(lua: Lua): String = toString()
     fun luaEquals(lua: Lua): Boolean = lua.toAny(1) == lua.toAny(2)
     fun luaTypeName(): String = javaClass.simpleName
