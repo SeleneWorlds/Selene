d0657b493ef2487f7747a05f8889a7872ff13803
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-29 08:39:03 +0200

    fix: Implement RegistryObject for SoundDefinition, use checkRegistry instead of checkString

--- common/src/main/kotlin/world/selene/common/data/SoundDefinition.kt
+++ common/src/main/kotlin/world/selene/common/data/SoundDefinition.kt
@@ -4,4 +4,18 @@ data class SoundDefinition(
     val audio: String,
     override val metadata: Map<String, Any> = emptyMap(),
     override val tags: Set<String> = emptySet()
-) : MetadataHolder, TagHolder
+) : MetadataHolder, TagHolder, RegistryObject<SoundDefinition> {
+    override var id: Int = 0; private set
+    override lateinit var name: String; private set
+    override lateinit var registry: Registry<SoundDefinition>; private set
+
+    override fun initializeFromRegistry(
+        registry: Registry<SoundDefinition>,
+        name: String,
+        id: Int
+    ) {
+        this.registry = registry
+        this.name = name
+        this.id = id
+    }
+}
--- server/src/main/kotlin/world/selene/server/lua/LuaSoundsModule.kt
+++ server/src/main/kotlin/world/selene/server/lua/LuaSoundsModule.kt
@@ -4,8 +4,8 @@ import party.iroiro.luajava.Lua
 import party.iroiro.luajava.value.LuaValue
 import world.selene.common.lua.LuaModule
 import world.selene.common.lua.checkCoordinate
+import world.selene.common.lua.checkRegistry
 import world.selene.common.lua.checkUserdata
-import world.selene.common.lua.checkString
 import world.selene.common.lua.checkType
 import world.selene.common.lua.getFieldFloat
 import world.selene.common.lua.register
@@ -29,21 +29,19 @@ class LuaSoundsModule(
 
     private fun luaPlaySound(lua: Lua): Int {
         val player = lua.checkUserdata<Player>(1)
-        val soundName = lua.checkString(2)
+        val sound = lua.checkRegistry(2, registries.sounds)
         if (lua.top >= 3) lua.checkType(3, Lua.LuaType.TABLE)
 
         val volume = lua.getFieldFloat(3, "volume") ?: 1f
         val pitch = lua.getFieldFloat(3, "pitch") ?: 1f
 
-        val soundId = registries.mappings.getId("sounds", soundName)
-            ?: return lua.error(IllegalArgumentException("Could not find sound with name $soundName"))
-        player.client.send(PlaySoundPacket(soundId, volume, pitch, null))
+        player.client.send(PlaySoundPacket(sound.id, volume, pitch, null))
         return 0
     }
 
     private fun luaPlaySoundAt(lua: Lua): Int {
         val (coordinate, index) = lua.checkCoordinate(1)
-        val soundName = lua.checkString(index + 1)
+        val sound = lua.checkRegistry(index + 1, registries.sounds)
         val dimension = if (lua.top >= index + 2) lua.checkUserdata(
             index + 2,
             Dimension::class
@@ -53,16 +51,13 @@ class LuaSoundsModule(
         val volume = lua.getFieldFloat(index + 2, "volume") ?: 1f
         val pitch = lua.getFieldFloat(index + 2, "pitch") ?: 1f
 
-        val soundId = registries.mappings.getId("sounds", soundName)
-            ?: return lua.error(IllegalArgumentException("Could not find sound with name $soundName"))
-
-        val packet = PlaySoundPacket(soundId, volume, pitch, coordinate)
+        val packet = PlaySoundPacket(sound.id, volume, pitch, coordinate)
         dimension.syncManager.sendToAllWatching(coordinate, packet)
         return 0
     }
 
     private fun luaPlayGlobalSound(lua: Lua): Int {
-        val soundName = lua.checkString(1)
+        val sound = lua.checkRegistry(1, registries.sounds)
         val dimension = if (lua.top >= 2) lua.checkUserdata(
             2,
             Dimension::class
@@ -72,9 +67,7 @@ class LuaSoundsModule(
         val volume = lua.getFieldFloat(2, "volume") ?: 1f
         val pitch = lua.getFieldFloat(2, "pitch") ?: 1f
 
-        val soundId = registries.mappings.getId("sounds", soundName)
-            ?: return lua.error(IllegalArgumentException("Could not find sound with name $soundName"))
-        dimension.syncManager.sendToAll(PlaySoundPacket(soundId, volume, pitch, null))
+        dimension.syncManager.sendToAll(PlaySoundPacket(sound.id, volume, pitch, null))
         return 0
     }
 
