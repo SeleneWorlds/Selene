a628668e69e6687d4a8cdc8a4e83d44d6a4da12b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-02 12:35:06 +0200

    refactor: wip rendering rewrite

--- client/src/main/kotlin/world/selene/client/rendering/animator/DrawableAnimator.kt
+++ client/src/main/kotlin/world/selene/client/rendering/animator/DrawableAnimator.kt
@@ -4,9 +4,15 @@ import world.selene.client.rendering.drawable.AnimatedDrawable
 import world.selene.client.rendering.drawable.Drawable
 
 class DrawableAnimator : Animator {
-    fun addAnimation(key: String, drawable: AnimatedDrawable) {
-        TODO("Not yet implemented")
+
+    private val animations = mutableMapOf<String, AnimatedDrawable>()
+
+    val drawable: Drawable? get() {
+        return null
     }
 
-    val drawable: Drawable
+    fun addAnimation(key: String, drawable: AnimatedDrawable) {
+        animations[key] = drawable
+    }
+
 }
