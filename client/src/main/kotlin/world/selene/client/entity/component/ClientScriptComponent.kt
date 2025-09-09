b4e7ede1eaedcb04163f1f8485b41276ebfbaf26
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-09 20:15:26 +0200

    fix: Use observable map for client script components, only error once if module require fails

--- client/src/main/kotlin/world/selene/client/entity/component/ClientScriptComponent.kt
+++ client/src/main/kotlin/world/selene/client/entity/component/ClientScriptComponent.kt
@@ -3,44 +3,44 @@ package world.selene.client.entity.component
 import org.koin.mp.KoinPlatform
 import party.iroiro.luajava.Lua
 import party.iroiro.luajava.LuaException
-import party.iroiro.luajava.value.LuaValue
 import world.selene.client.maps.Entity
 import world.selene.common.lua.LuaManager
 import world.selene.common.lua.LuaTrace
-import world.selene.common.lua.newTable
+import world.selene.common.lua.ObservableMap
 import world.selene.common.lua.xpCall
 
 class ClientScriptComponent(val moduleName: String) : EntityComponent, TickableComponent,
     LuaTrace {
     private val luaManager: LuaManager by KoinPlatform.getKoin().inject()
-    private var module: LuaValue? = null
-    private var data: LuaValue? = null
+    private var initialized = false
+    private var enabled = true
+    private val data = ObservableMap()
 
     override fun update(entity: Entity, delta: Float) {
+        if (!enabled) return
         val lua = luaManager.lua
-        val initPending = module == null
-        val data = data ?: lua.newTable {}.also {
-            data = it
-        }
         lua.getGlobal("require")
         lua.push(moduleName)
         try {
             lua.pCall(1, 1)
-        } catch (_: LuaException) {
+        } catch (e: LuaException) {
+            e.printStackTrace()
+            enabled = false
             return
         }
-        if (initPending) {
+        if (!initialized) {
             lua.getField(-1, "Initialize")
             if (lua.isFunction(-1)) {
                 lua.push(entity, Lua.Conversion.NONE)
-                lua.push(data)
+                lua.push(data, Lua.Conversion.NONE)
                 lua.xpCall(2, 0, this)
             } else lua.pop(1)
+            initialized = true
         }
         lua.getField(-1, "TickEntity")
         if (lua.isFunction(-1)) {
             lua.push(entity, Lua.Conversion.NONE)
-            lua.push(data)
+            lua.push(data, Lua.Conversion.NONE)
             lua.push(delta)
             lua.xpCall(3, 0, this)
         } else lua.pop(1)
