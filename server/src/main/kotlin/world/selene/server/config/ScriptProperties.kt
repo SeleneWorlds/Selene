72c50a6e13c6b764ecf01d2ab1b4150b479a502e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-09 17:32:34 +0200

    feat: Add support for loading script properties from SELENE_SCRIPT_ envs

--- server/src/main/kotlin/world/selene/server/config/ScriptProperties.kt
+++ server/src/main/kotlin/world/selene/server/config/ScriptProperties.kt
@@ -20,6 +20,19 @@ class ScriptProperties {
     }
 
     fun getProperty(key: String): String? {
+        val upperCamelCaseKey = key.toUpperCamelCase()
+        val envValue = System.getenv("SELENE_SCRIPT_$upperCamelCaseKey")
+        if (envValue != null) {
+            return envValue
+        }
         return properties.getProperty(key)
+    }
+
+    private fun String.toUpperCamelCase(): String {
+        return this
+            .replace(Regex("([a-z])([A-Z])"), "$1_$2")
+            .replace(".", "_")
+            .replace("-", "_")
+            .uppercase()
     }
 }
