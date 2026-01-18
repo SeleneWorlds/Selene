1c345e4145a2e70c4a72722cfcb57ca0fa6c71db
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-18 12:04:32 +0100

    build: Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.21" apply false
-    kotlin("plugin.serialization") version "2.1.20" apply false
+    kotlin("jvm") version "2.2.20" apply false
+    kotlin("plugin.serialization") version "2.3.0" apply false
 }
 
 allprojects {
