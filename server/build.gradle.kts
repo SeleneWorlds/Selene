984bfce1b7b212dc4ce6b5a6566274df0655a920
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-18 12:04:59 +0100

    build: Update Gradle Shadow Plugin

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     kotlin("jvm")
     application
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.3.1"
 }
 
 application {
