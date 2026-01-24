159e408ad4af94f0cb13be2a5e758eae19878655
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-24 11:27:28 +0100

    feat: Add RegistryOwnedObject.asReference

--- common/src/main/kotlin/world/selene/common/data/RegistryOwnedObject.kt
+++ common/src/main/kotlin/world/selene/common/data/RegistryOwnedObject.kt
@@ -3,4 +3,6 @@ package world.selene.common.data
 interface RegistryOwnedObject<T : Any> {
     val registry: Registry<T>
     val identifier: Identifier
+
+    val asReference get() = RegistryReference.ByIdentifier(registry, identifier)
 }
