d2c6723611ba4febd3fafc270e11ac612f0e88a2
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-25 22:22:25 +0100

    feat: Add AssetReloadListener

--- client/src/main/kotlin/world/selene/client/assets/AssetProvider.kt
+++ client/src/main/kotlin/world/selene/client/assets/AssetProvider.kt
@@ -15,6 +15,7 @@ class AssetProvider(private val logger: Logger, private val assetStorage: AssetS
     val missingTexture = TextureRegion(Texture(1, 1, Pixmap.Format.RGBA8888))
 
     private val assetSubscriptions = ConcurrentHashMap<String, MutableSet<(String) -> Unit>>()
+    private val reloadListeners = mutableSetOf<AssetReloadListener>()
 
     fun loadTexture(texturePath: String): Texture? {
         try {
@@ -45,10 +46,33 @@ class AssetProvider(private val logger: Logger, private val assetStorage: AssetS
         assetSubscriptions[assetPath]?.forEach { callback ->
             callback(assetPath)
         }
+        notifyAssetChangedListeners(assetPath)
     }
 
     override fun dispose() {
         assetStorage.dispose()
         assetSubscriptions.clear()
+        reloadListeners.clear()
     }
+
+    /**
+     * Registers a reload listener to receive notifications about asset changes.
+     */
+    fun addReloadListener(listener: AssetReloadListener) {
+        reloadListeners.add(listener)
+    }
+
+    /**
+     * Removes a previously registered reload listener.
+     */
+    fun removeReloadListener(listener: AssetReloadListener) {
+        reloadListeners.remove(listener)
+    }
+
+    private fun notifyAssetChangedListeners(assetPath: String) {
+        reloadListeners.forEach { listener ->
+            listener.onAssetChanged(assetPath)
+        }
+    }
+
 }
