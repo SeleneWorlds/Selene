6ff08981654505e22177dafead821d9128729e0c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-06 16:18:37 +0200

    feat: Add Stage#screenToStage

--- client/src/main/kotlin/com/seleneworlds/client/ui/lua/StageLuaMetatable.kt
+++ client/src/main/kotlin/com/seleneworlds/client/ui/lua/StageLuaMetatable.kt
@@ -1,5 +1,6 @@
 package com.seleneworlds.client.ui.lua
 
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.scenes.scene2d.Stage
 import party.iroiro.luajava.Lua
 import com.seleneworlds.common.lua.LuaMappedMetatable
@@ -9,6 +10,7 @@ import com.seleneworlds.common.lua.util.checkUserdata
 object StageLuaMetatable {
     val luaMeta = LuaMappedMetatable(Stage::class) {
         callable(::luaHit)
+        callable(::luaScreenToStage)
     }
 
     private fun luaHit(lua: Lua): Int {
@@ -19,5 +21,15 @@ object StageLuaMetatable {
         val actor = stage.hit(stageX, stageY, touchable)
         lua.push(actor, Lua.Conversion.NONE)
         return 1
+    }
+
+    private fun luaScreenToStage(lua: Lua): Int {
+        val stage = lua.checkUserdata<Stage>(1)
+        val screenX = lua.checkFloat(2)
+        val screenY = lua.checkFloat(3)
+        val stageCoords = stage.viewport.unproject(Vector3(screenX, screenY, 0f))
+        lua.push(stageCoords.x)
+        lua.push(stageCoords.y)
+        return 2
     }
 }
