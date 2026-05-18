8f643aaaa7168fe325fbaa1ff3410ba128e18e19
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-18 22:51:38 +0200

    fix: Fix auth broker issuer

--- server/src/main/kotlin/com/seleneworlds/server/config/SystemConfig.kt
+++ server/src/main/kotlin/com/seleneworlds/server/config/SystemConfig.kt
@@ -2,6 +2,6 @@ package com.seleneworlds.server.config
 
 data class SystemConfig(
     val heartbeatServer: String = "https://telescope.seleneworlds.com",
-    val authBrokerIssuer: String = "https://auth-broker.seleneworlds.com",
+    val authBrokerIssuer: String = "selene-auth-broker",
     val authBrokerJwksUrl: String = "https://auth-broker.seleneworlds.com/.well-known/jwks.json"
 )
