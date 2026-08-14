2c04313ffa7b97264a5042d3e0032d6275c9164b
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-14 20:28:50 +0200

    feat: Expose setWrap and setAlignment to Lua UI

--- client/src/main/kotlin/com/seleneworlds/client/ui/lua/LabelLuaMetatable.kt
+++ client/src/main/kotlin/com/seleneworlds/client/ui/lua/LabelLuaMetatable.kt
@@ -1,14 +1,19 @@
 package com.seleneworlds.client.ui.lua
 
 import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.utils.Align
 import party.iroiro.luajava.Lua
+import com.seleneworlds.common.lua.util.checkBoolean
 import com.seleneworlds.common.lua.util.checkString
 import com.seleneworlds.common.lua.util.checkUserdata
+import com.seleneworlds.common.lua.util.throwError
 
 object LabelLuaMetatable {
     val luaMeta = ActorLuaMetatable.luaMeta.extend(Label::class) {
         callable(::getText)
         callable(::setText)
+        callable(::setWrap)
+        callable(::setAlignment)
     }
 
     private fun getText(lua: Lua): Int {
@@ -22,5 +27,60 @@ object LabelLuaMetatable {
         val text = lua.checkString(2)
         label.setText(text)
         return 0
+    }
+
+    /**
+     * ```signatures
+     * SetWrap(wrap: boolean)
+     * ```
+     */
+    private fun setWrap(lua: Lua): Int {
+        val label = lua.checkUserdata<Label>(1)
+        label.setWrap(lua.checkBoolean(2))
+        return 0
+    }
+
+    /**
+     * ```signatures
+     * SetAlignment(alignment: string, lineAlignment: string|nil)
+     * ```
+     */
+    private fun setAlignment(lua: Lua): Int {
+        val label = lua.checkUserdata<Label>(1)
+        val labelAlignment = lua.checkAlignment(2)
+        if (lua.isString(3)) {
+            label.setAlignment(labelAlignment, lua.checkLineAlignment(3))
+        } else {
+            label.setAlignment(labelAlignment)
+        }
+        return 0
+    }
+
+    private fun Lua.checkAlignment(index: Int): Int {
+        return when (val alignment = checkString(index).normalizedAlignmentName()) {
+            "topleft", "lefttop" -> Align.topLeft
+            "top" -> Align.top
+            "topright", "righttop" -> Align.topRight
+            "left" -> Align.left
+            "center", "centre" -> Align.center
+            "right" -> Align.right
+            "bottomleft", "leftbottom" -> Align.bottomLeft
+            "bottom" -> Align.bottom
+            "bottomright", "rightbottom" -> Align.bottomRight
+            else -> throwError("Unknown label alignment '$alignment'")
+        }
+    }
+
+    private fun Lua.checkLineAlignment(index: Int): Int {
+        return when (val alignment = checkString(index).normalizedAlignmentName()) {
+            "left" -> Align.left
+            "center", "centre" -> Align.center
+            "right" -> Align.right
+            else -> throwError("Unknown label line alignment '$alignment'")
+        }
+    }
+
+    private fun String.normalizedAlignmentName(): String {
+        return lowercase().filter(Char::isLetter)
     }
 }
