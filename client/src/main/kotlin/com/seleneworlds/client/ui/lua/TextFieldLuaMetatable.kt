fd2e036fa94f80d635567fa9b52d508d219415ed
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-14 18:49:08 +0200

    feat: Expose TextField getMessageText (i.e. getPlaceholder) and onChanged to Lua

--- client/src/main/kotlin/com/seleneworlds/client/ui/lua/TextFieldLuaMetatable.kt
+++ client/src/main/kotlin/com/seleneworlds/client/ui/lua/TextFieldLuaMetatable.kt
@@ -1,15 +1,27 @@
 package com.seleneworlds.client.ui.lua
 
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.TextField
-import party.iroiro.luajava.Lua
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
+import com.seleneworlds.common.lua.util.checkFunction
 import com.seleneworlds.common.lua.util.checkString
 import com.seleneworlds.common.lua.util.checkUserdata
+import com.seleneworlds.common.lua.util.getCallerInfo
+import com.seleneworlds.common.lua.util.xpCall
+import com.seleneworlds.common.script.ConstantTrace
+import org.slf4j.LoggerFactory
+import party.iroiro.luajava.Lua
+import party.iroiro.luajava.LuaException
+import party.iroiro.luajava.value.LuaValue
 
 object TextFieldLuaMetatable {
     val luaMeta = ActorLuaMetatable.luaMeta.extend(TextField::class) {
         callable(::getInputListener)
         callable(::getText)
         callable(::setText)
+        callable(::getMessageText)
+        callable(::setMessageText)
+        callable(::onChanged)
     }
 
     private fun getInputListener(lua: Lua): Int {
@@ -30,4 +42,43 @@ object TextFieldLuaMetatable {
         textField.setText(text)
         return 0
     }
+
+    private fun getMessageText(lua: Lua): Int {
+        val textField = lua.checkUserdata<TextField>(1)
+        textField.messageText?.let(lua::push) ?: lua.pushNil()
+        return 1
+    }
+
+    private fun setMessageText(lua: Lua): Int {
+        val textField = lua.checkUserdata<TextField>(1)
+        textField.messageText = if (lua.isNil(2)) null else lua.checkString(2)
+        return 0
+    }
+
+    private fun onChanged(lua: Lua): Int {
+        val textField = lua.checkUserdata<TextField>(1)
+        val callback = lua.checkFunction(2)
+        val trace = ConstantTrace("[textField onChanged] registered in ${lua.getCallerInfo()}")
+
+        textField.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent, actor: Actor) {
+                runCallback(callback, trace, textField)
+            }
+        })
+        return 0
+    }
+
+    private fun runCallback(callback: LuaValue, trace: ConstantTrace, textField: TextField) {
+        val callbackLua = callback.state()
+        callbackLua.push(callback)
+        callbackLua.push(textField, Lua.Conversion.NONE)
+        callbackLua.push(textField.text)
+        try {
+            callbackLua.xpCall(2, 0, trace)
+        } catch (e: LuaException) {
+            logger.error("Lua error in text field change callback", e)
+        }
+    }
+
+    private val logger = LoggerFactory.getLogger(TextFieldLuaMetatable::class.java)
 }
