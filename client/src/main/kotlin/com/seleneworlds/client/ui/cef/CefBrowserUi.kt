cf13ef82c13b1160c6fb5f90b04a4ba28eb837e3
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-08-22 13:07:55 +0200

    refactor: Map log levels when logging CEF console logs

--- client/src/main/kotlin/com/seleneworlds/client/ui/cef/CefBrowserUi.kt
+++ client/src/main/kotlin/com/seleneworlds/client/ui/cef/CefBrowserUi.kt
@@ -19,12 +19,7 @@ import org.cef.CefClient
 import org.cef.CefSettings
 import org.cef.browser.CefBrowser
 import org.cef.browser.CefFrame
-import org.cef.handler.CefDisplayHandlerAdapter
-import org.cef.handler.CefLoadHandler
-import org.cef.handler.CefLoadHandlerAdapter
-import org.cef.handler.CefLifeSpanHandlerAdapter
-import org.cef.handler.CefRequestHandler
-import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.handler.*
 import org.cef.network.CefRequest
 import org.lwjgl.opengl.GL12
 import org.slf4j.Logger
@@ -116,9 +111,28 @@ class CefBrowserUi(
                 }
             })
             addDisplayHandler(object : CefDisplayHandlerAdapter() {
-                override fun onConsoleMessage(browser: CefBrowser, level: CefSettings.LogSeverity,
-                                              message: String, source: String, line: Int): Boolean {
-                    logger.info("CEF console [{}] {}:{} {}", level, source, line, message)
+                override fun onConsoleMessage(
+                    browser: CefBrowser, level: CefSettings.LogSeverity,
+                    message: String, source: String, line: Int
+                ): Boolean {
+                    when (level) {
+                        CefSettings.LogSeverity.LOGSEVERITY_ERROR,
+                        CefSettings.LogSeverity.LOGSEVERITY_FATAL -> logger.error(
+                            "CEF console {}:{} {}",
+                            source,
+                            line,
+                            message
+                        )
+
+                        CefSettings.LogSeverity.LOGSEVERITY_WARNING -> logger.warn(
+                            "CEF console {}:{} {}",
+                            source,
+                            line,
+                            message
+                        )
+
+                        else -> logger.info("CEF console {}:{} {}", source, line, message)
+                    }
                     return true
                 }
             })
